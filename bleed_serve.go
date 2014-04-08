0bbd718eab00c91ca546712be15751c4c781401c
Tom Gallacher <me@tomg.co>
2014-04-08 11:19:20 +0100

    Update bleed_serve.go

--- bleed_serve.go
+++ bleed_serve.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 )
 
-var PAYLOAD = []byte("heartbleed.filippo.io")
+var PAYLOAD = []byte(">>>>>>(((%$£^")
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "http://filippo.io/Heartbleed", http.StatusFound)
